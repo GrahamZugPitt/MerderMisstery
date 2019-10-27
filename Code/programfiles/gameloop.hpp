31f908a4fb6e77fe6fcb283a392f94a9abbaeec6
ErickBrindock <esb43@pitt.edu>
2019-10-26 22:48:01 -0400

    Adding code files back

---
+++ Code/programfiles/gameloop.hpp
@@ -0,0 +1,6 @@
+#ifndef GLOBALVAR_COMMON
+#define GLOBALVAR_COMMON
+#include "common.hpp"
+#endif
+
+void gameloop(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* renderer, bool farnan);
