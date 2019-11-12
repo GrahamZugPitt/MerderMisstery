214fe1cdc7620bb6a4e8fceec7b7043b5302eb9c
Dorian Vesely <dvesely40@pitt.edu>
2019-11-12 13:55:45 -0500

    credits should be good to go

--- Code/programfiles/credits.cpp
+++ Code/programfiles/credits.cpp
@@ -4,6 +4,8 @@
 SDL_Texture* gImg = nullptr;
 
 void roll_credits(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* renderer){
+  // inCredits boolean value
+  // bool inCredits = true;
 
   std::string dir = "Art/Credit Pictures/";
 	std::string ext = "CreditPicture.png";
@@ -11,17 +13,27 @@ void roll_credits(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* 
     "Darpun", "dves", "Griffin", "Jahnke", "Jiang", "ShawnBlake", "Zug"};
 	for(auto name: imgstrs){
 		std::string full = dir + name + ext;
-		// Using IMG_Load to load png images
-		// NOTE: the total lack of error checking
+
+    // load image files
 		gImg = loadFiles(full.c_str(), renderer);
-		// Apply the image
+
     SDL_RenderCopy(renderer, gImg, NULL, NULL);
-		// SDL_BlitSurface(gImg, NULL, gScreenSurface, NULL );
-		// Update the window surface
-		// SDL_UpdateWindowSurface(gWindow);
     SDL_RenderPresent(renderer);
-		// Wait 5 seconds
+
+		// Wait 3 seconds
 		SDL_Delay(3000);
+
+    // //Get the Keyboard State
+    // keyState = SDL_GetKeyboardState(NULL);
+    //
+    // // If user selects escape or enter keys, leave credits
+    // if (keyState[SDL_SCANCODE_ESCAPE] || keyState[SDL_SCANCODE_RETURN])
+    //   return;
+    //
+    // //Quit application
+    // if(e.type == SDL_QUIT)
+    //   *quit = true;
+
 	}
 
 }
