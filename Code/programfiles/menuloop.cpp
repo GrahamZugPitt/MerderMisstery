fef890f1f672b9d16a1c9559a5b249ae9bdec6f7
Dorian Vesely <dvesely40@pitt.edu>
2019-11-12 12:39:01 -0500

    game now starts based on mouse click, seed entry and credits to come

--- Code/programfiles/menuloop.cpp
+++ Code/programfiles/menuloop.cpp
@@ -5,53 +5,79 @@
 std::string logoImgPath = "Art/Logo/Start_Screen.png";
 std::string loadImgPath = "Art/Logo/Loading.png";
 
+const int ALL_BUTTON_TOP = 618;
+const int ALL_BUTTON_BOTTOM = 532;
+
 void menuloop(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* renderer){
   // Load the title screens
   SDL_Texture* logoScreen = loadFiles(logoImgPath, renderer);
   bool next = false;
 
+  int cursor_x;
+  int cursor_y;
+
   //Logo Screen Loop
   while(!next && !(*quit)) {
-      while(SDL_PollEvent(&e) != 0) {
-          if(e.type == SDL_QUIT) {
-            (*quit) = true;
-          }
-        }
-
-        //All Buttons top Y value: 540
-        //All Buttons bottom Y value: 626
-
-
-        //Enter Seed Left-X value: 96
-        //Enter Seed Right-X value: 386
-
-
-        //Start Game Left-X value: 485
-        //Start Game Right-X value: 774
-
-        //Roll Credits Left-X value: 875
-        //Roll Credits Right-X value: 1163
-
-
-        // Get the Keyboard State
-        keyState = SDL_GetKeyboardState(NULL);
-
-        // // Start the game
-        // if (keyState[SDL_SCANCODE_SPACE])
-        //   next = true;
-
-        // Open Chat room
-        if (keyState[SDL_SCANCODE_C])
-            enter_chat(e, &(*quit), keyState, renderer);
-
-        // Quit the game
-        if (keyState[SDL_SCANCODE_Q])
-            (*quit) = true;
-
-        //Logo Screen Render
-        SDL_RenderCopy(renderer, logoScreen, NULL, NULL);
-        SDL_RenderPresent(renderer);
+    while(SDL_PollEvent(&e) != 0) {
+      if(e.type == SDL_QUIT) {
+        (*quit) = true;
+      }
     }
+
+    // if mouse is clicked and released
+    if(e.type == SDL_MOUSEBUTTONUP) {
+      // Get mouse position
+      cursor_x = e.motion.x;
+      cursor_y = e.motion.y;
+      // std::cout << "motion.x:" << cursor_x << "\n";
+      // std::cout << "motion.y:" << cursor_y << "\n";
+      // std::cout << "mouse clicked\n";
+
+      // ALL_BUTTON_TOP Y value: 532
+      // ALL_BUTTON_BOTTOM Y value: 618
+      // check if click was in Y range of buttons
+      // if it wasn't in Y range, no need to check X range
+      if((cursor_y > ALL_BUTTON_BOTTOM) && (cursor_y < ALL_BUTTON_TOP))
+      {
+        // cursor_x value comparisons to button ranges
+        switch(cursor_x){
+          //Enter Seed Left-X value: 107
+          //Enter Seed Right-X value: 393
+          case 107 ... 393:
+            std::cout << "Enter Seed";
+            break;
+
+          //Start Game Left-X value: 492
+          //Start Game Right-X value: 779
+          case 492 ... 779:
+            std::cout << "Start Game";
+            next = true; // Start the game
+            break;
+
+          //Roll Credits Left-X value: 876
+          //Roll Credits Right-X value: 1164
+          case 876 ... 1164:
+            std::cout << "End Scene";
+            break;
+        }
+      }
+    }
+
+    // Get the Keyboard State
+    keyState = SDL_GetKeyboardState(NULL);
+
+    // Open Chat room
+    if (keyState[SDL_SCANCODE_C])
+        enter_chat(e, &(*quit), keyState, renderer);
+
+    // Quit the game
+    if (keyState[SDL_SCANCODE_Q])
+        (*quit) = true;
+
+    //Logo Screen Render
+    SDL_RenderCopy(renderer, logoScreen, NULL, NULL);
+    SDL_RenderPresent(renderer);
+  }
 
   SDL_Texture* loadScreen = loadFiles(loadImgPath, renderer);
   SDL_RenderCopy(renderer, loadScreen, NULL, NULL);
