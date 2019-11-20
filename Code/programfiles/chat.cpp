2c27c4a8837ace284af474805377871050e2729e
Dorian Vesely <dvesely40@pitt.edu>
2019-11-20 00:26:34 -0500

    added in message rect for previous messages that can be set from server

--- Code/programfiles/chat.cpp
+++ Code/programfiles/chat.cpp
@@ -12,7 +12,7 @@ void enter_chat(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* re
   SDL_Texture* chatScreen = loadFiles(chatScreenPath, renderer);
 
   //Render chat template
-  SDL_RenderCopy(renderer, chatScreen, NULL, NULL);
+  // SDL_RenderCopy(renderer, chatScreen, NULL, NULL); //dont need this one
 
   //User input box left X: 32
   //User input box right X: 1247
@@ -26,7 +26,7 @@ void enter_chat(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* re
   std::string inputText = "";
 
   //Open a font style and sets size
-  TTF_Font* fontStyle = TTF_OpenFont("Fonts/Arial.ttf", 25);
+  TTF_Font* fontStyle = TTF_OpenFont("Fonts/Arial.ttf", 35);
   if(!fontStyle) {
       printf("TTF_OpenFont: %s\n", TTF_GetError());
   }
@@ -34,33 +34,62 @@ void enter_chat(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* re
   //Set White variable in rgb format
   SDL_Color textWhite = {255, 255, 255};
 
+  //Read in a file from the server, Colton ;)
+  //Need to make sure to append \n (new line) to the end of each message so that
+  //it reads in like the example text below
+
+  //take past messages from server and make them a surface --> texture
+  std::string textFromServer = "User1: this is from the server kinda but not really "\
+   "becuaase i am just typing a bunch of shit to try and see if the text will "\
+   "wrap to the next line \nUser2: Thats dope! \nUser3: Rad! \nUser4: The cooliest"\
+   "\nUser5: The cooliest \nUser6: The cooliest \nUser7: The cooliest \nUser8: The cooliest";
+   /////// THIS ABOVE STRING CAN BE REPLACED BY A STRING READING FILE INPUT FROM THE SERVER////////
+
+  //convert server text into 'message' char needed for TTF_RenderText_Blended_Wrapped
+  char message[textFromServer.size() + 1];
+	std::copy(textFromServer.begin(), textFromServer.end(), message);
+	message[textFromServer.size()] = '\0';
+
   //TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
   SDL_Surface* surfaceInputText = TTF_RenderText_Solid(fontStyle, inputText.c_str(), textWhite);
+  SDL_Surface* surfaceMessage = TTF_RenderText_Blended_Wrapped(fontStyle, message, textWhite, 1215);
 
-  //now you can convert it into a texture
+
+  //now you can convert it into a texture for input and previous messages
   SDL_Texture* textureInputText = SDL_CreateTextureFromSurface(renderer, surfaceInputText);
+  SDL_Texture* textureMessage = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 
-  SDL_Rect inputRect; //create a rect
+  //create a rect for the textures to appear in
+  SDL_Rect inputRect;
+  SDL_Rect serverMessageRect;
 
   int textW = 0;
   int textH = 0;
   //returns inherent width and height of font determed by size set in fontStyle variable
   //stores these values in textH and textW variables
+  //rect for input text
   SDL_QueryTexture(textureInputText, NULL, NULL, &textW, &textH);
-
   inputRect.x = 36;  //controls the rect's x coordinate
   inputRect.y = 597; // controls the rect's y coordinte
   inputRect.w = textW; //1215; // controls the width of the rect
   inputRect.h = textH; //113; // controls the height of the rect
-  //text will most likely still go outside the input box, need to clean up with
-  //example from https://gamedev.stackexchange.com/questions/140294/what-is-the-most-efficient-way-to-render-a-textbox-in-c-sdl2
 
+  //rect for messages from server
+  SDL_QueryTexture(textureMessage, NULL, NULL, &textW, &textH);
+  serverMessageRect.x = 36;
+  serverMessageRect.y = 120;
+  serverMessageRect.w = textW;
+  serverMessageRect.h = textH;
+
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, chatScreen, NULL, NULL);
+  SDL_RenderCopy(renderer, textureMessage, NULL, &serverMessageRect);
   SDL_RenderCopy(renderer, textureInputText, NULL, &inputRect);
   SDL_RenderSetClipRect(renderer, NULL);
   SDL_RenderPresent(renderer);
 
   // set render draw color to be the same as the input box color
-  // will be used to fill a blank rectangle to cover up old text 
+  // will be used to fill a blank rectangle to cover up old text
   SDL_SetRenderDrawColor(renderer, 143, 171, 221, 0);
 
   SDL_Rect blankRect; // create a rect to be used to display a blank box
@@ -124,7 +153,7 @@ void enter_chat(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* re
     }
 
     //Rerender text if needed
-    if(renderText) { 
+    if(renderText) {
 
       //Text is not empty
       if(inputText != "") {
@@ -138,12 +167,12 @@ void enter_chat(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* re
         textureInputText = SDL_CreateTextureFromSurface(renderer, surfaceInputText);
       }
       SDL_QueryTexture(textureInputText, NULL, NULL, &textW, &textH);
-      inputRect.w = textW; 
+      inputRect.w = textW;
       inputRect.h = textH;
       blankRect.h = textH;
       if(textW>blankRect.w)
         blankRect.w = textW; // controls the width of the rect
-      
+
       SDL_RenderDrawRect(renderer, &blankRect);
       SDL_RenderFillRect(renderer, &blankRect);
       SDL_RenderPresent(renderer);
@@ -181,5 +210,5 @@ void enter_chat(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* re
     }
   }
 
-  
+
 }
