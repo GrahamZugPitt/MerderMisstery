ed4c29d3adcc16ee5539bee4ee23516ac49803f2
Dorian Vesely <dvesely40@pitt.edu>
2019-11-29 11:08:52 -0500

    updated chat.cpp structure to separate out functionality to methods

--- Code/programfiles/chat.hpp
+++ Code/programfiles/chat.hpp
@@ -1,3 +1,6 @@
 #include "common.hpp"
 
 void enter_chat(SDL_Event e, bool *quit, const Uint8 *keyState, SDL_Renderer* renderer);
+void initialize_input(SDL_Renderer* renderer);
+void update_messages(SDL_Renderer* renderer);
+void update_render(SDL_Renderer* renderer);
